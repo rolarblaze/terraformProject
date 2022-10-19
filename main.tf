ba563bc93e4aa15149ad2757e9f3b1fb0f2a3f32
rolarblaze <rolarblaze@github.com>
2022-10-19 14:45:28 +0100

    adding route table to our resources using terraform

--- main.tf
+++ main.tf
@@ -23,3 +23,19 @@ resource "aws_internet_gateway" "jenkins_internet_gateway" {
   }
 
 }
+
+resource "aws_route_table" "jenkins_public_rt" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+
+  tags = {
+    "Name" = "public-route-table"
+  }
+
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.jenkins_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.jenkins_internet_gateway.id
+
+}
